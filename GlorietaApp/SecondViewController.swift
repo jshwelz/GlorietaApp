35e517a9e3de7c389d827d70735a9e82d374179b
Joshua Welchez <jshwelz@Joshuas-MacBook-Pro.local>
2017-09-18 15:40:47 -0600

    Initial Commit

---
+++ GlorietaApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GlorietaApp
+//
+//  Created by Joshua Welchez on 9/18/17.
+//  Copyright © 2017 Joshua Welchez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
